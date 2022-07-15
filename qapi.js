23edc26a6873875241ac43569e95413fcd986be3
oneforall <109364618+oneforallwordpress@users.noreply.github.com>
2022-07-15 22:38:37 +0200

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"62d1d00449c9db002347406d",
+				"62d1d00449c9db0023474070"
+			]
+		},
+		"62d1d00449c9db002347406d": {
+			"id": "62d1d00449c9db002347406d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"62d1d00449c9db0023474070": {
+			"id": "62d1d00449c9db0023474070",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
